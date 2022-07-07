0a787e005d0171a5d7ab5f58763f53f6c5e52167
Nayara Nogueira Escudeiro Nascimento <nayara.nascimento@darede.com.br>
2022-07-07 14:31:07 -0300

    feat: muda banco

--- src/functions/pokemon/create.js
+++ src/functions/pokemon/create.js
@@ -1,8 +1,34 @@
+import mysql from "mysql2/promise";
+
 export const handler = async (event, context) => {
-    try {
-      return 'foi cadastro pokemon';
-    } catch (error) {
-      return 'deu ruim';
-    }
-  };
-  
+  try {
+    const connection = await mysql.createConnection({
+      host: "lupa-tyr.com.br",
+      user: "u157627371_desavio",
+      database: "u157627371_desavio",
+      password: "!@Desafio123#$"
+    })
+
+    const nome = "Pikachuchu"
+    const geracao = 1
+    const ataque = 99
+    const tipoPokemon = "Elétrico"
+
+    const [rows, fields] = await connection.execute(`INSERT INTO u157627371_desavio.pokemons
+      (
+      nome,
+      geracao,
+      ataque,
+      tipoPokemon)
+    VALUES
+        (
+    '${nome}',
+     ${geracao},
+     ${ataque},
+    '${tipoPokemon}');`)
+    return JSON.stringify(rows);
+  } catch (error) {
+    return error.message;
+  }
+};
+
--- src/functions/pokemon/list.js
+++ src/functions/pokemon/list.js
@@ -1,102 +1,23 @@
-import sql from "mssql";
-
-const config = {
-  user: process.env.sa,
-  password: process.env.darede2022,
-  server: 'localhost',
-  database: process.env.desafio,
-
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 30000
-  },
-  options: {
-    encrypt: true,
-    // instanceName: 'SQLEXPRESS'
-  },
-  port: 1433
-}
-
+import mysql from "mysql2/promise";
 
 export const handler = async (event, context) => {
   try {
-    await sql.connect(config)
-    const resultado = await sql.query`SELECT * FROM pokemon`
-    return resultado;
+    const connection = await mysql.createConnection({
+      host: "lupa-tyr.com.br",
+      user: "u157627371_desavio",
+      database: "u157627371_desavio",
+      password: "!@Desafio123#$"
+    })
+
+    const nome = "Pikachuchu"
+    const geracao = 1
+    const ataque = 99
+    const tipoPokemon = "Elétrico"
+
+    const [rows, fields] = await connection.execute('SELECT * FROM u157627371_desavio.pokemons')
+    return JSON.stringify(rows);
   } catch (error) {
     return error.message;
   }
 };
 
-// import sql from "mssql"
-// const config = {
-//   server: 'localhost',
-//   port: 1433,
-//   user:process.env.sa,
-//   password:process.env.darede2022,
-//   database:process.env.desafio,
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000
-//   },
-// }
-// export const handler = async (event, context) => {
-//   try {
-//     // make sure that any items are correctly URL encoded in the connection string
-//     await sql.connect(config)
-//     const result = await sql.query`select * from pokemon`
-//     console.dir(result)
-//    } catch (err) {
-//     // ... error checks
-//    }
-// };
-
-// // const config = {
-// //   user: 'sa',
-// //   password: 'darede@2022',
-// //   server: 'DAREDE-001079',
-// //   database: 'desafio',
-// //   options: {
-// //     trustedconnection: true,
-// //     enableArithAbort: true,
-// //     instancename: "",
-// //   },
-// //   port: 1433
-// // };
-
-// // async () => {
-// //   try {
-// //    // make sure that any items are correctly URL encoded in the connection string
-// //    await sql.connect(config)
-// //    const result = await sql.query`select * from pokemon`
-// //    console.dir(result)
-// //   } catch (err) {
-// //    // ... error checks
-// //   }
-// // }
-
-
-// // async function listPokemon() {
-// //   try {
-// //     let pool = await sql.connect(config)
-// //     let resultado = await pool.request().query('select * from pokemon')
-// //     console.log(resultado)
-// //     sql.close();
-// //   } catch (error) {
-// //     console.log(err.message)
-// //   }
-// // }
-// // module.exports ={
-// //   getPokemons : getPokemons
-// // }
-// // export const handler = async (event, context) => {
-// //   try {
-// //     await sql.connect(config)
-
-// //     return (resultado);
-// //   } catch (error) {
-// //     return error.message;
-// //   }
-// // };
